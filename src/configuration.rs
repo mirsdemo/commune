3b050a590f1525c188d659078d7bf5dd7a076342
mirsdemo <mirsdemo@facilitative.space>
2026-07-13 09:43:16 -0400

    Feature: Implement configuration file.

---
+++ src/configuration.rs
@@ -0,0 +1,11 @@
+// Configuration
+// Written by [@mirsdemo](https://www.github.com/mirsdemo)
+// 07/12/2026
+// Configuration file.
+
+pub const EXIT_TIMEOUT: u8 = 3; // In seconds.
+pub const MAIN_THREAD_THROTTLE: u8 = 64; // In milliseconds.
+pub const THREAD_TIMEOUT: u16 = 2_500; // In milliseconds.
+pub const THREAD_THROTTLE: u16 = 1_500; // In milliseconds.
+
+pub const STATE_FILE_PATH: &str = "scanner-state.json";
